6c479df4e3fd69e526fe2c4bc45c798762c4475b
straight-code <97550737+straight-code@users.noreply.github.com>
2022-03-22 22:06:01 +0000

    v2.3

--- script.js
+++ script.js
@@ -1,85 +1,120 @@
-document.querySelectorAll('.button').forEach(button => {
-
-    let duration = 3000,
-        svg = button.querySelector('svg'),
-        svgPath = new Proxy({
-            y: null,
-            smoothing: null
-        }, {
-            set(target, key, value) {
-                target[key] = value;
-                if(target.y !== null && target.smoothing !== null) {
-                    svg.innerHTML = getPath(target.y, target.smoothing, null);
-                }
-                return true;
-            },
-            get(target, key) {
-                return target[key];
-            }
-        });
-
-    button.style.setProperty('--duration', duration);
-
-    svgPath.y = 20;
-    svgPath.smoothing = 0;
-
-    button.addEventListener('click', e => {
-        
-        e.preventDefault();
-
-        if(!button.classList.contains('loading')) {
-
-            button.classList.add('loading');
-
-            gsap.to(svgPath, {
-                smoothing: .3,
-                duration: duration * .065 / 1000
-            });
-
-            gsap.to(svgPath, {
-                y: 12,
-                duration: duration * .265 / 1000,
-                delay: duration * .065 / 1000,
-                ease: Elastic.easeOut.config(1.12, .4)
-            });
-
-            setTimeout(() => {
-                svg.innerHTML = getPath(0, 0, [
-                    [3, 14],
-                    [8, 19],
-                    [21, 6]
-                ]);
-            }, duration / 2);
-
-        }
-
-    });
-
+$('#parallax').parallax({
+	invertX: true,
+	invertY: true,
+	scalarX: 15,
+	frictionY: .1
 });
 
-function getPoint(point, i, a, smoothing) {
-    let cp = (current, previous, next, reverse) => {
-            let p = previous || current,
-                n = next || current,
-                o = {
-                    length: Math.sqrt(Math.pow(n[0] - p[0], 2) + Math.pow(n[1] - p[1], 2)),
-                    angle: Math.atan2(n[1] - p[1], n[0] - p[0])
-                },
-                angle = o.angle + (reverse ? Math.PI : 0),
-                length = o.length * smoothing;
-            return [current[0] + Math.cos(angle) * length, current[1] + Math.sin(angle) * length];
-        },
-        cps = cp(a[i - 1], a[i - 2], point, false),
-        cpe = cp(point, a[i - 1], a[i + 1], true);
-    return `C ${cps[0]},${cps[1]} ${cpe[0]},${cpe[1]} ${point[0]},${point[1]}`;
-}
 
-function getPath(update, smoothing, pointsNew) {
-    let points = pointsNew ? pointsNew : [
-            [4, 12],
-            [12, update],
-            [20, 12]
-        ],
-        d = points.reduce((acc, point, i, a) => i === 0 ? `M ${point[0]},${point[1]}` : `${acc} ${getPoint(point, i, a, smoothing)}`, '');
-    return `<path d="${d}" />`;
-}
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 120,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+
+
